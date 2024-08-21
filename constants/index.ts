9c23ad8d2c1911468e8ade7f37ca31a0987965a8
HIEU TRUONG <aceblack0003@gmail.com>
2024-08-21 22:18:59 +0700

    sidebar

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: 'Trang chủ',
+        route: '/',
+        imgUrl: '/icons/Home.svg',
+    },
+    {
+        label: 'Sắp diễn ra',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },
+    {
+        label: 'Trước kia',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg',
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/video.svg',
+    },
+    {
+        label: 'Phòng cá nhân',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg',
+    },
+]
